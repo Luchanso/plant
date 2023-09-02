aaa33d6599d753d13725d1839b52c688e97eb61d
Oleg Loutchansky <luchansodev@gmail.com>
2023-09-02 03:14:36 +0300

    feat: add some serial port debug

--- collector/src/main.rs
+++ collector/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use console::Term;
+use dialoguer::{theme::ColorfulTheme, FuzzySelect};
+
+use std::time::Duration;
+
+mod port_info;
+
+fn main() -> std::io::Result<()> {
+    let items = vec!["Item 1", "item 2"];
+    let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
+        .items(&items)
+        .default(0)
+        .interact_on_opt(&Term::stderr())?;
+
+    match selection {
+        Some(index) => println!("User selected item : {}", items[index]),
+        None => println!("User did not select anything"),
+    }
+
+    port_info::print();
+
+    let mut port = serialport::new("COM3", 9_600)
+        .timeout(Duration::from_millis(10000))
+        .open()
+        .expect("Failed to open port");
+
+    let mut serial_buf: Vec<u8> = vec![0; 32];
+    port.read(serial_buf.as_mut_slice())
+        .expect("Found no data!");
+
+    let string: String = String::from_utf8(serial_buf).expect("Found invalid UTF-8");
+
+    println!("{:#?}", string);
+
+    Ok(())
 }
---
+++ collector/src/port_info.rs
@@ -0,0 +1,42 @@
+use serialport::SerialPortType;
+
+pub fn print() {
+    let ports = serialport::available_ports().expect("No ports found!");
+    for p in ports {
+        println!("  {}", p.port_name);
+        match p.port_type {
+            SerialPortType::UsbPort(info) => {
+                println!("    Type: USB");
+                println!("    VID:{:04x} PID:{:04x}", info.vid, info.pid);
+                println!(
+                    "     Serial Number: {}",
+                    info.serial_number.as_ref().map_or("", String::as_str)
+                );
+                println!(
+                    "      Manufacturer: {}",
+                    info.manufacturer.as_ref().map_or("", String::as_str)
+                );
+                println!(
+                    "           Product: {}",
+                    info.product.as_ref().map_or("", String::as_str)
+                );
+                #[cfg(feature = "usbportinfo-interface")]
+                println!(
+                    "         Interface: {}",
+                    info.interface
+                        .as_ref()
+                        .map_or("".to_string(), |x| format!("{:02x}", *x))
+                );
+            }
+            SerialPortType::BluetoothPort => {
+                println!("    Type: Bluetooth");
+            }
+            SerialPortType::PciPort => {
+                println!("    Type: PCI");
+            }
+            SerialPortType::Unknown => {
+                println!("    Type: Unknown");
+            }
+        }
+    }
+}
