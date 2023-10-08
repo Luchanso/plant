b454ada8529ec635046a63f7aabb219555fdf1b3
Oleg Loutchansky <luchansodev@gmail.com>
2023-10-08 15:34:47 +0300

    feat: add serial-async

--- collector/src/main.rs
+++ collector/src/main.rs
@@ -4,10 +4,9 @@ mod api;
 mod metrics;
 mod serial;
 
-
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
-    tokio::join!(api::run(), serial::run());
+    let _ = tokio::join!(api::run(), serial::run());
 }
--- collector/src/serial/mod.rs
+++ collector/src/serial/mod.rs
@@ -1,63 +1,64 @@
-use bytes::BytesMut;
+use std::str::from_utf8;
+
+use bytes::{Buf, BytesMut};
+use crc;
+use serialport::SerialPort;
 use tokio::{
-    io::AsyncReadExt,
+    io::{AsyncReadExt, AsyncWriteExt},
     time::{sleep, Duration},
 };
 use tokio_cron_scheduler::{Job, JobScheduler, JobSchedulerError};
-use tokio_serial::{SerialPortBuilderExt, SerialStream};
+use tokio_serial::SerialPortBuilderExt;
 
 mod path;
 mod port_info;
 
 const BAUD_RATE: u32 = 9600;
-const MIN_MSG_SIZE: u32 = 4;
 
-async fn measurement_request(mut port: SerialStream) {
-    // port.try_write();
-}
+const CRC: crc::Crc<u8> = crc::Crc::<u8>::new(&crc::CRC_8_MAXIM_DOW);
 
-pub async fn run() {
+async fn measurement_request() {
+    let message: Vec<u8> = vec![0x3A, 0x01, 0x00];
+    let checksum = CRC.checksum(message.as_slice());
+    let request_message = [message, vec![checksum]].concat();
+    let request_message = request_message.as_slice();
+
     let path = path::get();
+    let mut serial_stream = tokio_serial::new(path, BAUD_RATE).open_native_async();
 
-    sleep(Duration::from_secs(1)).await;
+    println!("Open");
 
-    let _ = schedule().await;
+    match serial_stream {
+        Ok(mut stream) => {
+            stream.write(request_message).await.unwrap();
+            println!("Wrote {:?}", request_message);
+            // stream.write_buf(&mut request_message).await.unwrap();
 
-    let mut port = tokio_serial::new(path, BAUD_RATE)
-        .open_native_async()
-        .unwrap();
-
-    loop {
-        let mut buffer = BytesMut::with_capacity(10);
-        let _ = port.read_buf(&mut buffer).await;
-
-        println!("{:?}", &buffer[..]);
-    }
-
-    return;
-
-    // let mut port = serialport::new(path, BAUD_RATE).open();
-
-    // match port {
-    //     Err(error) => {
-    //         println!("Cannot open serialport: {:?}", error);
-    //         port_info::print();
-    //     }
-    //     Ok(result) => read(result).await,
-    // };
+            println!("Waiting");
+            let mut read_buf = BytesMut::with_capacity(32);
+            stream.read_buf(&mut read_buf).await.unwrap();
+            println!("result {:?}", read_buf.to_vec());
+        }
+        Err(error) => {
+            println!("Cannot open serialport: {:?}", error);
+            port_info::print();
+        }
+    };
 }
 
-async fn schedule() -> Result<(), JobSchedulerError> {
-    let mut sched = JobScheduler::new().await?;
+pub async fn run() -> Result<(), JobSchedulerError> {
+    measurement_request().await;
+    // let scheduler = JobScheduler::new().await?;
+    // let job = Job::new_async("1/3 * * * * *", move |_uuid, mut job| {
+    //     Box::pin(async move {
+    //         let _ = job.shutdown().await;
+    //         measurement_request().await;
+    //     })
+    // })
+    // .unwrap();
 
-    // Add basic cron job
-    sched.add(
-        Job::new("1/10 * * * * *", |_uuid, _l| {
-            println!("I run every 10 seconds");
-        })?
-    ).await?;
-
-    sched.start().await?;
+    // scheduler.add(job).await?;
+    // scheduler.start().await?;
 
     Ok(())
 }
