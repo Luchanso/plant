018517bd1e289199083845055c99ec055fefb1a1
BortEngineer <dmitriy2109@gmai.com>
2023-12-19 22:44:15 +0100

    Improve toolchain; code reformat; add ETL library

--- hardware/src/time_struct.h
+++ hardware/src/time_struct.h
@@ -3,11 +3,13 @@
 
 // Just a definition of the time struct
 
+#include <stdint.h>
+
 typedef struct {
-  uint8_t year; //year of the century, i.e. can have value from 0 to 99
+  uint8_t year; // year of the century, i.e. can have value from 0 to 99
   uint8_t month;
-  uint8_t dayOfMonth; //can have value from 1 to 31
-  uint8_t dayOfWeek; //can have value from 1 to 7, from monday to sunday
+  uint8_t dayOfMonth; // can have value from 1 to 31
+  uint8_t dayOfWeek;  // can have value from 1 to 7, from monday to sunday
   uint8_t hours;
   uint8_t minutes;
   uint8_t seconds;
